f2fcbf88fa684a243233e01cc960258a7d374ac7
FrostyPaw <vitolsartur@gmail.com>
2026-05-02 09:46:38 +0300

    Create GuestExistsRule.cs

---
+++ Hotel_Management_System_2/Modules/ReservationCreation/GuestExistsRule.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using System.Linq;
+using Hotel_Management_System_2.Models;
+
+namespace Hotel_Management_System_2.Modules.ReservationCreation
+{
+    public class GuestExistsRule : IReservationRule
+    {
+        public ReservationResult Validate(
+            ReservationRequest request,
+            List<Guest> guests,
+            List<Room> rooms,
+            List<Reservation> reservations)
+        {
+            bool guestExists = guests.Any(g => g.Id == request.GuestId);
+
+            if (!guestExists)
+            {
+                return ReservationResult.Fail("Guest does not exist.");
+            }
+
+            return ReservationResult.Ok("Guest exists.");
+        }
+    }
+}
